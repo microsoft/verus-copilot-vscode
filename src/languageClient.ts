0e1fedb4170b1459468ff19f5746314f4bff8449
Qinzheng Sun <qinsu@microsoft.com>
2024-10-24 17:15:25 +0800

    language client restart

--- src/languageClient.ts
+++ src/languageClient.ts
@@ -1,18 +1,34 @@
+import * as vscode from 'vscode'
 import * as lc from "vscode-languageclient/node.js"
 import { getRustAnalyzerConfig, getConfigValue, RUST_ANALYZER_SERVER_PATH } from './config.js'
 
 let _client: lc.LanguageClient | null = null
+let _prevPath: string | null = null
+let _messageSent = false
 
 export const getLanguageClient = () => {
-    if (_client != null) {
-        return _client
-    }
-
     const path = getConfigValue<string>(RUST_ANALYZER_SERVER_PATH)
-    if (path == null) {
-        throw Error('verus analyzer is not installed')
-    }
-	
+	if (_client != null && _client.state !== lc.State.Stopped) {
+		if (path === _prevPath) {
+			return _client
+		} else {
+			_client.dispose()
+			_client = null
+		}
+	}
+
+	if (path == null || path === _prevPath) {
+		if (!_messageSent) {
+			vscode.window.showErrorMessage('Can not connect to verus analyzer, please check the user setting "rust-analyzer.server.path"')
+			_messageSent = true
+		}
+		throw new Error('Failed to get language client.')
+	} else {
+		// path changed, reset message flag
+		_messageSent = false
+	}
+
+	_prevPath = path
     _client = new lc.LanguageClient(
 		'verus-copilot.verus-analyzer.lc',
 		{ command: path },
