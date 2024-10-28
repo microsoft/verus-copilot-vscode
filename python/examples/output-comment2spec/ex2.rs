4b37fa192f2f0457b04fe434061aa43a8a85a231
arsene1995 <arsene1995@users.noreply.github.com>
2024-10-28 14:15:54 -0700

    revise spec-synthesis example
    the extra "as int" is unnecessary and unhelpful

--- python/examples/output-comment2spec/ex2.rs
+++ python/examples/output-comment2spec/ex2.rs
@@ -3,8 +3,8 @@
  * postcondition: every element in result vector R is larger than 20; the length of R is the same as the length of v
  *  requires
  *      v@.len()>0,
- *      forall |i:int| 0<= i < v@.len() ==> v[i as int] < 10,
+ *      forall |i:int| 0<= i < v@.len() ==> v[i] < 10,
  *  ensures
- *      forall |i:int| 0<= i < R@.len() ==> R[i as int] > 20,
+ *      forall |i:int| 0<= i < R@.len() ==> R[i] > 20,
  *      R@.len() == v@.len(),
  */
